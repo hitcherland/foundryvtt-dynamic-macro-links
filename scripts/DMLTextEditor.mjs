3d389325424ee8afc8bc393f17805d6c24d563b4
william-isley-3rd <william.isley.3rd@gmail.com>
2022-12-02 23:48:27 -0500

    Label Fixes
    Fix for journal labels not showing up properly for custom labels. Updated structure of link generation functionality.

--- scripts/DMLTextEditor.mjs
+++ scripts/DMLTextEditor.mjs
@@ -19,14 +19,22 @@ class DMLTextEditor extends TextEditor {
             console.warn("The 'entities' option for TextEditor.enrichHTML is deprecated. Please use 'documents' instead.");
             documents = options.entities;
         }
-
+		
+		// This section now only does dynamic macro links! 
         if (documents) {
+
             if (updateTextArray) text = this._getTextNodes(html);
             const macroNames = game.settings.get('dynamic-macro-links', 'dml-pairs').map(x => x.document);
-            const documentTypes = CONST.DOCUMENT_LINK_TYPES.concat("UUID", "Compendium", ...macroNames);
-            const rgx = new RegExp(`@(${documentTypes.join("|")})\\[([^\\]]+)\\](?:{([^}]+)})?`, 'g');
+            // const documentTypes = CONST.DOCUMENT_LINK_TYPES.concat("UUID", "Compendium", ...macroNames);
+            // const rgx = new RegExp(`@(${documentTypes.join("|")})\\[([^\\]]+)\\](?:{([^}]+)})?`, 'g');
+			const rgx = new RegExp(`@(${macroNames.join("|")})\\[([^\\]]+)\\](?:{([^}]+)})?`, 'g');
             updateTextArray = this._replaceTextContent(text, rgx, this._createContentLink);
         }
+		// Makes links for everything that isn't one of our dynamic macro links
+		if (documents){
+			if (updateTextArray) text = this._getTextNodes(html);
+			updateTextArray = super._enrichContentLinks(text);
+		}
 
         // Replace hyperlinks
         if (links) {
@@ -48,29 +56,29 @@ class DMLTextEditor extends TextEditor {
     };
 
 
-    static _createContentLink([match, type, target, name]) {
+    static _createContentLink([match, type, target, macro_match_name, link_match_name]) {
 		// Prepare replacement data
         const data = {
             cls: ["entity-link", "content-link"],
             icon: null,
             dataset: {},
-            name: name
+            name: macro_match_name
         };
         let broken = false;
         const macros = game.settings.get('dynamic-macro-links', 'dml-pairs');
         const macroDict = Object.assign({}, ...macros.map(x => ({[x.document]: x.macroId})));
 		
-		if ((macroDict[type] === undefined) && (target[0] === '.')){
-			// super._createContentLink breaks when sending a relative path. 
-			// There's probably a better way to do this... 
-			let temp_id = target.substring(1);
-			let link_parent = game.journal.find(j => j.pages.some(p => p.id===temp_id));
-			let page_id = link_parent.pages.find(p => p.id===temp_id);
-			return super._createContentLink([match, type, page_id.uuid, name]);
-		}
+	if ((macroDict[type] === undefined) && (target[0] === '.')){
+		// super._createContentLink breaks when sending a relative path. 
+		// There's probably a better way to do this... 
+		let temp_id = target.substring(1);
+		let link_parent = game.journal.find(j => j.pages.some(p => p.id===temp_id));
+		let page_id = link_parent.pages.find(p => p.id===temp_id);
+		return super._createContentLink([match, type, page_id.uuid, macro_match_name, link_match_name]);
+	}
         else if (macroDict[type] === undefined) {
 			// Use the normal way to construct links. Works for link that are not relative. 
-            return super._createContentLink([match, type, target, name]);
+            return super._createContentLink([match, type, target, macro_match_name, link_match_name]);
         }
 		
 
